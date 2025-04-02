fec1e9e556184a0d3952bf37dafc0dbfd2e2cb66
Arandi Lopez <arandilopez.93@gmail.com>
2025-04-02 14:18:28 -0600

    feat: support for vitest and graphql

--- lua/plugins/test.lua
+++ lua/plugins/test.lua
@@ -5,10 +5,20 @@ return {
     dependencies = {
       "zidhuss/neotest-minitest",
       "olimorris/neotest-rspec",
+      "marilari88/neotest-vitest",
     },
     config = function()
       require("neotest").setup({
         adapters = {
+          require("neotest-vitest")({
+            is_test_file = function(file_path)
+              -- Check if the project is "my-peculiar-project"
+              if string.match(file_path, "rxv-frontend") then
+                return string.match(file_path, ".*%.vi%.test")
+              end
+              return false
+            end,
+          }),
           require("neotest-rspec"),
           require("neotest-minitest")({
             test_cmd = function()
