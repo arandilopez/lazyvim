1cba750baf7a9523ac5856dd9a16a80c7bb85ec9
Arandi Lopez <arandilopez.93@gmail.com>
2025-04-07 20:14:31 -0600

    chore: remove temporal fix of copilot.lua

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -3,12 +3,4 @@ return {
     "CopilotC-Nvim/CopilotChat.nvim",
     build = "make tiktoken", -- Only on MacOS or Linux
   },
-  -- Temporal fix for copilot.lua
-  {
-    "zbirenbaum/copilot.lua",
-    optional = true,
-    opts = function()
-      require("copilot.api").status = require("copilot.status")
-    end,
-  },
 }
