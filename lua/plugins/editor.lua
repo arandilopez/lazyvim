f3e3cc041d3d8d0d83a9eb55a445d991e375faf9
Arandi Lopez <arandilopez.93@gmail.com>
2025-04-09 17:36:13 -0600

    feat(editor): adjustments to UI and git blame

---
+++ lua/plugins/editor.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    "lewis6991/gitsigns.nvim",
+    enabled = true,
+    opts = {
+      auto_attach = true,
+      current_line_blame = true, -- Toggle with `:Gitsigns toggle_current_line_blame`
+      current_line_blame_opts = {
+        virt_text = true,
+        virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+        delay = 1000,
+        ignore_whitespace = false,
+        virt_text_priority = 100,
+        use_focus = true,
+      },
+      current_line_blame_formatter = "<author>, <author_time:%R>",
+    },
+  },
+}
--- lua/plugins/ui.lua
+++ lua/plugins/ui.lua
@@ -11,12 +11,41 @@ return {
     },
   },
   {
+    "ibhagwan/fzf-lua",
+    opts = {
+      winopts = {
+        layout = "vertical",
+        preview = {
+          layout = "vertical",
+        },
+      },
+    },
+  },
+  {
     "nvim-lualine/lualine.nvim",
     opts = {
       sections = {
         lualine_z = {
           { "fileformat", separator = " ", padding = { left = 1, right = 0 } },
           { "encoding", padding = { left = 0, right = 1 } },
+        },
+      },
+    },
+  },
+
+  {
+    "snacks.nvim",
+    opts = {
+      dashboard = {
+        preset = {
+          header = [[
+███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+   ]],
         },
       },
     },
