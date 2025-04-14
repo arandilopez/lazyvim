19f5b1b8680008b4b14dd870113058dd05dc4e50
Arandi Lopez <arandilopez.93@gmail.com>
2025-04-14 12:14:53 -0600

    feat: custom keymaps to yank filenames

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,7 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Files
+vim.keymap.set("n", "<leader>fy", '<cmd>let @+ = expand("%:t")<cr>', { desc = "Yank file name" })
+vim.keymap.set("n", "<leader>fY", '<cmd>let @+ = expand("%")<cr>', { desc = "Yank file pathname" })
