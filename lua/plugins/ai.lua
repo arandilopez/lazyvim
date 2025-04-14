f136d6d69cd68abdc3a070868c024e2ac529fe9d
Arandi Lopez <arandilopez.93@gmail.com>
2025-04-14 12:14:01 -0600

    fix: copilot chat to render markdown in buffer

--- lua/plugins/ai.lua
+++ lua/plugins/ai.lua
@@ -6,4 +6,12 @@ return {
       model = "claude-3.7-sonnet",
     },
   },
+  {
+    "MeanderingProgrammer/render-markdown.nvim",
+    optional = true,
+    opts = {
+      file_types = { "markdown", "copilot-chat" },
+    },
+    ft = { "markdown", "norg", "rmd", "org", "codecompanion", "copilot-chat" },
+  },
 }
