9b0bfc2c0c8f8887b97a1993c39a46aef0b6f5c4
Arandi Lopez <arandilopez.93@gmail.com>
2025-04-14 12:15:36 -0600

    feat: custom keymaps to insert lines above and below the cursor without leaving normal mode

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -5,3 +5,14 @@
 -- Files
 vim.keymap.set("n", "<leader>fy", '<cmd>let @+ = expand("%:t")<cr>', { desc = "Yank file name" })
 vim.keymap.set("n", "<leader>fY", '<cmd>let @+ = expand("%")<cr>', { desc = "Yank file pathname" })
+
+-- Editor
+vim.keymap.set("n", "]o", function()
+  local line = vim.fn.line(".")
+  vim.fn.append(line, "")
+end, { desc = "Insert line below the cursor" })
+
+vim.keymap.set("n", "[o", function()
+  local line = vim.fn.line(".")
+  vim.fn.append(line - 1, "")
+end, { desc = "Insert line above the cursor" })
