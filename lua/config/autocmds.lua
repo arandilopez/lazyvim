3417994d094f13c66bca4b1b333340fdb8760ace
Arandi Lopez <arandilopez.93@gmail.com>
2025-07-08 20:05:43 -0600

    feat: disable format on save for text.erb file

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,12 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Disable formating for text.erb views.
+-- Erb Formatter works well for html.erb but not for plain text views like mailers
+vim.api.nvim_create_autocmd({ "BufEnter" }, {
+  pattern = { "*.text.erb" },
+  callback = function()
+    vim.b.autoformat = false
+  end,
+})
