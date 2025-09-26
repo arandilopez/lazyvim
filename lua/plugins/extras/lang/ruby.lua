7bde61e39b790e558f50ad8288984dafb477ef53
Arandi Lopez <arandilopez.93@gmail.com>
2025-09-26 09:00:02 -0600

    feat: enable herb-ls

--- lua/plugins/extras/lang/ruby.lua
+++ lua/plugins/extras/lang/ruby.lua
@@ -10,11 +10,14 @@ return {
         stimulus_ls = {
           enabled = lsp == "ruby_lsp",
         },
+        herb_ls = {
+          enabled = true,
+        },
       },
     },
   },
   {
     "mason-org/mason.nvim",
-    opts = { ensure_installed = { "stimulus-language-server" } },
+    opts = { ensure_installed = { "stimulus-language-server", "herb-language-server" } },
   },
 }
